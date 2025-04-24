fe197840ed2c0b72358a247963b20af06168e8f9
Antoine WEBER <antoine.weber@spiess.fr>
2025-04-24 11:27:39 +0200

    fix: Correction import useToast dans WorkloadPage

--- client/src/pages/WorkloadPage.tsx
+++ client/src/pages/WorkloadPage.tsx
@@ -4,7 +4,7 @@ import TaskListSidebar from '@/components/Workload/TaskListSidebar';
 import WorkloadCalendar from '@/components/Workload/WorkloadCalendar';
 import TaskForm from '@/components/Workload/TaskForm';
 import { Task } from '@shared/types/workload';
-import { useToast } from "@/components/ui/use-toast";
+import { useToast } from "@/components/ui/toast";
 import { formatISO, parse, addHours } from 'date-fns';
 import { DragDropContext, DropResult } from 'react-beautiful-dnd';
 import { Button } from '@/components/ui/button'; // Potentiellement nécessaire
