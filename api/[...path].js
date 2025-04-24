1765c5977e0e2317283c197ac26d0a4c2cfaf6bb
Antoine WEBER <antoine.weber@spiess.fr>
2025-04-24 13:10:53 +0200

    Refactorisation de l'API pour Vercel serverless functions

---
+++ api/[...path].js
@@ -0,0 +1,4 @@
+// api/[...path].js - Route catch-all pour toutes les requêtes API
+import handler from './vercel.js';
+
+export default handler; 
---
+++ api/index.js
@@ -0,0 +1,4 @@
+// api/index.js - Point d'entrée simplifié pour l'API
+import handler from './vercel.js';
+
+export default handler; 
