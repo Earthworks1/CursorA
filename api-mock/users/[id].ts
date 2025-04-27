8b9b9e7baf4b171e372a651f09978aa1ca729d34
Antoine WEBER <aweber67530@gmail.com>
2025-04-27 21:29:55 +0200

    feat(mock): ajout et adaptation des mocks pour tous les endpoints API (GET, POST, PATCH, PUT, DELETE)

--- api-mock/users/[id].ts
+++ api-mock/users/[id].ts
@@ -3,9 +3,8 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 export default function handler(req: VercelRequest, res: VercelResponse) {
   switch (req.method) {
     case 'GET':
-      res.status(200).json([]);
+      res.status(200).json({});
       break;
-    case 'POST':
     case 'PATCH':
     case 'PUT':
       res.status(200).json({ success: true });
