f3a4f6ec8b8fab0dded271e94ea362b2f511d5ad
Antoine WEBER <aweber67530@gmail.com>
2025-05-09 10:40:00 +0200

    feat: ajout des composants temporaires Gantt et Heatmap pour débloquer le build

---
+++ client/src/components/Heatmap.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Task } from "@/types";
+
+export default function Heatmap({ tasks, startDate, endDate, onCellClick }: { tasks: Task[], startDate: Date, endDate: Date, onCellClick: any }) {
+  return <div>Heatmap temporaire (à remplacer)</div>;
+} 
