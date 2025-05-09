d11be507c315f9da6ad13b31a7a24b533bcb9289
Antoine WEBER <aweber67530@gmail.com>
2025-05-09 09:58:11 +0200

    fix: update TacheDetail component usage in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -237,9 +237,9 @@ function Router() {
         )}
       </Route>
       <Route path="/taches/:id">
-        {(params: any) => (
+        {() => (
           <Layout>
-            <TacheDetails id={params.id} />
+            <TacheDetail />
           </Layout>
         )}
       </Route>
